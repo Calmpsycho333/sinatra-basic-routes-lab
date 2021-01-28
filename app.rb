a4ff5fc96eb6d417ef8217e6210f3177d86d5b86
calmpsycho333 <wrathking75@yahoo.com>
2021-01-28 14:14:25 -0500

    commit tree 9a14d746adfcc133cc6c62d879da31a75f006313

--- app.rb
+++ app.rb
@@ -1,4 +1,30 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+"Hello, World!"
+    end
+
+    
+    get '/name' do
+        " returns a 200 status code"
+    end
+
+    get '/name' do
+        "returns a 200 status code"
+        "My name is "
+
+    end
+
+        get '/hometown' do
+            "returns a 200 status code"
+            "My home town is "
+        end
+
+        get '/favorite-song' do
+            "returns a 200 status code"
+            "My favorite song is "
+        end
+    
 end
